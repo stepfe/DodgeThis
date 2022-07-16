d6b9a72b766b3ae76e59b5ae6b47046b1c5fa318
stepfe <stepfedorov@yandex.ru>
2022-07-16 20:56:47 +0300

    Добавлена точка входа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args){
+import game.UI.MainFrame;
+import game.controller.IController;
+import game.controller.KeyController;
+import game.logic.Game;
+import game.logic.IGame;
 
+/**
+ * Точка входа в приложение
+ */
+public class Main {
+
+    private static IGame game;
+    private static IController controller;
+    private static MainFrame frame;
+
+    public static void main(String[] args){
+        game = new Game();
+        controller = new KeyController(game);
+        frame = new MainFrame(controller);
+        game.addGamePaintListener(frame);
+        game.start();
     }
 }
