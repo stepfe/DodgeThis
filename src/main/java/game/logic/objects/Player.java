8aaa436ca5b96bfd992a7c17ae4e781a9d2f40db
stepfe <stepfedorov@yandex.ru>
2022-07-16 20:52:52 +0300

    Изменен игрок

--- src/main/java/game/logic/objects/Player.java
+++ src/main/java/game/logic/objects/Player.java
@@ -2,22 +2,59 @@ package game.logic.objects;
 
 import game.logic.objects.bullets.AbstractBulletFactory;
 
+import java.awt.image.BufferedImage;
+
+/**
+ * Корабль игрока
+ */
 public class Player extends Ship{
 
-    public static final String LEFT = "L";
-    public static final String RIGHT = "R";
-    public static final String UP = "U";
-    public static final String DOWN = "D";
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int UP = 2;
+    public static final int DOWN = 3;
 
-    public Player(int x, int y, int hp, int speed, AbstractBulletFactory bulletFactory) {
-        super(x, y, hp, speed, bulletFactory);
+    public Player(int x, int y, int hp, int speed, AbstractBulletFactory bulletFactory, BufferedImage image) {
+        super(x, y, hp, speed, bulletFactory, image);
     }
 
-    public void move(String direction){
-        //TODO Движение игрока
+    /**
+     * Движение игрока в заданном направлении
+     * @param direction направление
+     */
+    public void move(int direction) {
+        switch (direction) {
+            case LEFT : moveLeft();
+                break;
+            case RIGHT: moveRight();
+                break;
+            case UP: moveUp();
+                break;
+            case DOWN: moveDown();
+                break;
+        }
     }
 
-    public boolean isAlive(){
+    /**
+     * @return Жив ли игрок в зависимости от его здоровья
+     */
+    public boolean isAlive() {
         return hp > 0;
+    }
+
+    protected void moveLeft() {
+        this.x--;
+    }
+
+    protected void moveRight() {
+        this.x++;
+    }
+
+    protected void moveUp() {
+        this.y--;
+    }
+
+    protected void moveDown() {
+        this.y++;
     }
 }
